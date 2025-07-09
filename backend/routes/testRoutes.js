ffc414b46abced091dee5691bffca013394e6acb
Ahmet Tuna POTUR <tpotur@gmail.com>
2025-07-09 21:22:59 +0300

    isLoggedIn

--- backend/routes/testRoutes.js
+++ backend/routes/testRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const moment = require('moment');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  const isLoggedIn = req.cookies.jwt ? true : false;
+
   res.status(200).json({
     title: 'Test Request',
     data: new Date().toLocaleString(),
+    formatDate: moment(new Date()).format('D dddd MMMM YYYY HH:mm:ss'),
+    isLoggedIn,
   });
 });
 
